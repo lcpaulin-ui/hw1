40a7594e6d3b70d864708124daa204627b916f60
Lucia Paulino <lcpaulin@usc.edu>
2025-09-07 22:33:59 -0700

    1033pm fri

--- split.cpp
+++ split.cpp
@@ -21,49 +21,30 @@ struct Node
 #include "split.h"
 
 /* Add a prototype for a helper function here if you need */
-//void addNode(Node*& in, Node*& odds, Node*& evens);
+void addNode(Node*& in, Node*& list);
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
     /* Add code here */
-  // WRITE YOUR CODE HERE
+  // WRITE YOUR CODE HERE 
 
-  // empty case 
-  if(in == nullptr){
+  // empty case
+  if (in == nullptr){
     return;
   }
 
-  else if (in->value % 2 == 0){
-
-    if(evens == nullptr) {
-      evens = in;
-      evens->next = nullptr; 
-    }
-    else {
-      Node* temp = evens;
-      evens = in;
-      evens->next = temp; 
-    }
-  
-  }
-
-  else {
-    if (odds == nullptr){
-      odds = in;
-      odds->next = nullptr;
-    }
-
-    else{
-      Node* temp = odds;
-      odds = in;
-      odds->next = temp;
-    }
-  }
-
+  // take my ptr out of og list aka move head ptr to next pointer 
+  Node* curr = in;
   in = in->next; 
 
-  return split(in, odds, evens); 
+  else if (curr->value % 2 == 0){
+    addNode(curr, evens);
+  }
+  else {
+    addNode(curr, odds);
+  }
 
+  return split(in, odds, evens);
 
 }
 
@@ -71,14 +52,24 @@ void split(Node*& in, Node*& odds, Node*& evens)
 // helper function to traverse the list to the end, to add the new node.
 
 // pass in head pointer, use it to traverse to list's end and point to inserted node
-/**
-void addNode(Node*& head, Node*& odds, Node*& evens){
-  Node* temp = head; 
-  // get a pointer to next node 
-  Node* newHead = head; 
 
-  else if (head->value % 2 == 0){
-    
+void addNode(Node*& add, Node*& list){ 
+
+  // empty list case 
+  if (list == nullptr){
+    list = add;
+    list->next = nullptr; 
   }
- }
-*/
+
+  // got to the end case 
+  else if (list->next == nullptr){
+    list->next = add;
+    add->next = nullptr;
+    return; 
+  }
+
+  else {
+    addNode(add, list->next);
+  }
+
+}
